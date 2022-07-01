e3c762615c1cb521e2d2682b3175198c5b98e8d8
rusheekeshjadhav <rusheekesh@caizin.com>
2022-07-01 12:03:31 +0530

    rmq single pub-sub

---
+++ RabbitMQ_Consumer/RabbitMQ_Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ_Consumer
+{
+    static class Program
+    {
+        static void Main(String[] args)
+        {
+            // create connection factory
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+                // rmq listen on port 5672
+                // "amqp://{{username}}:{{password}}@{{host}}:{{port}}"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();   // creates and return a fresh channel, session and model
+
+            channel.QueueDeclare("test01",
+                durable: true,  // messege will stay until a consumer reads it
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);  // takes IModel as an argument in a constructor
+            consumer.Received += (sender, args) =>
+            {
+                var body = args.Body.ToArray();    // return byte array of messeges
+                var messege = Encoding.UTF8.GetString(body);
+                Console.WriteLine(messege); // printing json string as it is
+            };
+
+            channel.BasicConsume("test01", true, consumer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ_Producer/RabbitMQ_Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ_Producer
+{
+    static class Program
+    {
+        static void Main(String[] args)
+        {
+            // create connection factory
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+                // rmq listen on port 5672
+                // "amqp://{{username}}:{{password}}@{{host}}:{{port}}"
+            };
+
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();   // creates and return a fresh channel, session and model
+
+            channel.QueueDeclare("test01",
+                durable: true,  // messege will stay until a consumer reads it
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var messege = new { Sender = "Producer_01", Messege = "Hello from Producer_01 !!!" };    // annonymus object
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messege));    // converting JSON object into byte array by JsonConvert
+
+            channel.BasicPublish("", "test01", null, body);
+            // currently writing exchange as empty.
+            // routing key = name of the queue.
+        }
+    }
+}
